3a2fc29d92acdab5d66310eba921774616122985
Nathan <nathanbenson.affiliate@gmail.com>
2021-03-23 15:01:43 +0100

    first build

---
+++ src/fetch-function.js
@@ -0,0 +1,69 @@
+export let weatherData = {
+
+};
+
+
+export function fetchWeather (city) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=72c205fe50ec0c81fbf1577dbf8e83f0&units=metric`)
+    .then(response => {
+        return response.json();
+
+    }).then(response => {
+       console.log(response)
+    })
+    .catch(error => {
+        alert(error);
+
+    });
+};
+
+export function fetchWeather2 (city) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=72c205fe50ec0c81fbf1577dbf8e83f0&units=metric`)
+        .then(response => {
+            return response.json();
+
+        }).then(response => {
+            weatherData.name = response.name;
+            weatherData.description = response.weather[0].description;
+            weatherData.iconData = response.weather[0].icon;
+            weatherData.temp = Math.round(response.main.temp);
+            weatherData.feels_like = Math.round(response.main.feels_like);
+            weatherData.humidity = response.main.humidity;
+            weatherData.wind = Math.round(response.wind.speed);
+            weatherData.minTemp = Math.round(response.main.temp_min);
+            weatherData.maxTemp = Math.round(response.main.temp_max);
+        })
+        .catch(error => {
+            console.log(error);
+
+        });
+
+ };
+
+
+
+ export function displayWeather() {
+     const cityName = document.querySelector('.weather-city');
+     const description = document.querySelector('.weather-description');
+     const weatherIcon = document.querySelector('.weather-icon');
+     const weatherTemp = document.querySelector('.weather-temp');
+     const feelsLike = document.querySelector('#feels-like');
+     const humidity = document.querySelector('#humidity');
+     const wind = document.querySelector('#wind');
+     const minTemp = document.querySelector('#min-temp');
+     const maxTemp = document.querySelector('#max-temp');
+     
+     weatherTemp.textContent = weatherData.temp;
+     description.textContent = weatherData.description;
+     weatherIcon.style = `background: url("http://openweathermap.org/img/wn/${weatherData.iconData}@2x.png") no-repeat;`
+     feelsLike.textContent = weatherData.feels_like;
+     humidity.textContent = weatherData.humidity;
+     wind.textContent = weatherData.wind;
+     cityName.textContent = weatherData.name;
+     minTemp.textContent = weatherData.minTemp;
+     maxTemp.textContent = weatherData.maxTemp;
+
+ }
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import "./main.css";
+import "./header.css";
+import { fetchWeather, weatherData, fetchWeather2, displayWeather } from "./fetch-function";
 
-console.log('hello from webpack');
+
+const searchIcon = document.querySelector('#search-icon');
+const searchInput = document.querySelector('#search-input');
+
+function fetchDisplayWeather () {
+    fetchWeather2(searchInput.value);
+    setInterval(function() {
+        displayWeather();
+    }, 2000);
+    searchInput.value = '';
+}
+
+
+
+searchIcon.addEventListener('click', fetchDisplayWeather);
+searchInput.addEventListener('keypress', (e) =>{
+    if(e.key === 'Enter') {
+        fetchDisplayWeather();
+    }
+
+})
+
+// fetchWeather('london');
+// fetchWeather2('marbella');
+// console.log(weatherData);
+
+
+
+// setInterval(function() {
+//     displayWeather();
+// }, 4000);
+
+
